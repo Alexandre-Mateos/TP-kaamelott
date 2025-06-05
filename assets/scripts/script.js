334498123f35e9636e8f4bf4e46b477c9c7b3fa3
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-05 10:00:55 +0200

    ajout fonction creation carte avec requete API, ajout fonction listener au click

---
+++ assets/scripts/script.js
@@ -0,0 +1,29 @@
+let bouton = document.querySelector("#nouvelle-replique");
+let affichage = document.querySelector("#affichage-replique");
+
+
+bouton.addEventListener("click", () => {
+fetch(`https://kaamelott.xyz/api/v1/quote/random`).then((response) => {
+    return response.json();
+}).then((data) => {
+    dipslayCard(data);
+})
+})
+
+
+
+function dipslayCard (data){
+    let card = document.createElement("div");
+    let cardBody = document.createElement("div");
+
+    let auteur = document.createElement("h5");
+    auteur.innerHTML = data.author;
+    cardBody.insertAdjacentElement("beforeend", auteur);
+
+    let replique = document.createElement("p");
+    replique.innerHTML = data.content;
+    cardBody.insertAdjacentElement("beforeend", replique);
+
+    card.insertAdjacentElement("beforeend", cardBody);
+    affichage.insertAdjacentElement("beforeend", card);
+}
